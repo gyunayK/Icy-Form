fb3351fe15cd5af7b591f8787da44505234b27ac
Gyunay Kadirov <gyunay@hotmail.dk>
2024-04-21 18:03:25 -0700

    fix: vercel deployment error

--- backend/src/routes/form.router.ts
+++ backend/src/routes/form.router.ts
@@ -1,19 +1,19 @@
 import express, { Router } from 'express'
 import multer from 'multer'
 import {
-  postTechnology,
-  getTechnologyRequests,
-  getUrlTextContent,
+  // postTechnology,
+  // getTechnologyRequests,
+  // getUrlTextContent,
   scrapeUrlForMetadata
 } from '../controllers/form.controller'
 
 const router: Router = express.Router()
 const upload = multer({ dest: 'uploads/' })
 
-router
-  .route('/form')
-  .post(upload.fields([{ name: 'lightIcon' }, { name: 'darkIcon' }]), postTechnology)
-  .get(getTechnologyRequests)
+// router
+//   .route('/form')
+//   .post(upload.fields([{ name: 'lightIcon' }, { name: 'darkIcon' }]), postTechnology)
+//   .get(getTechnologyRequests)
 
 router.post('/form/technology/', scrapeUrlForMetadata)
 
